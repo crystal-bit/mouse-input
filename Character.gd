6e1c2d7b4a71ce8e607c18c6822d2ca4c5e64005
Davide Cristini <davcri91@gmail.com>
2019-08-28 22:10:19 +0100

    Redo the tutorial with simpler and cleaner code

--- Character.gd
+++ Character.gd
@@ -6,19 +6,50 @@ var mouse_pressed = false
 
 signal angle_changed
 
-func set_mouse_hover(val):
-	mouse_hover = val
-	emit_signal("mouse_hover_changed", val)
+# debug variable
+var draw_axis = false
 
-func set_angle(val):
-	"""val in rads"""
-	rotation = val
-	emit_signal("angle_changed", rotation)
 	
 func _input(event):
-	pass
+	if event is InputEventMouseMotion:
+		# Calculate the vector from the character to the current mouse position
+		var directionToMouse: Vector2 = get_local_mouse_position()
+		# var directionToMouse: Vector2 = get_local_mouse_position() - global_position
+		# Set the angle (in radians)
+		rotate(directionToMouse.angle())
+		# Shorter alternative in Godot:	
+		# rotation = get_global_mouse_position().angle_to_point(global_position)
+		# clamp the rotation
+		if abs(rotation / (2 * PI)) > 1:
+			if rotation > 0:
+				rotation = rotation - (2*PI)
+			else:
+				rotation = rotation + (2*PI)
+		# send value to the GUI
+		emit_signal("angle_changed", rotation)
+	if event is InputEventMouseButton:
+		if event.is_action_pressed("mouse_lmb"):
+			mouse_pressed = true
+		elif event.is_action_released("mouse_lmb"):
+			mouse_pressed = false
 	# request the node a re-draw (calls the _draw() method)
 	update()
 
 func _draw():
-	pass
+	if mouse_pressed:
+		print(get_local_mouse_position().ceil().y)
+		draw_line(Vector2(), get_local_mouse_position(), Color.blue)
+	
+	if draw_axis:
+		draw_line(Vector2(), Vector2(500, 0), Color.red)
+		draw_line(Vector2(), Vector2(0, 500), Color.green)
+
+
+func _on_Area2D_mouse_entered():
+	$AnimationPlayer.play("squash")
+	$AnimationPlayer.queue("squash-loop")
+	mouse_hover = true
+
+func _on_Area2D_mouse_exited():
+	$AnimationPlayer.play("squash-out")
+	mouse_hover = false
--- GUIControls.gd
+++ GUIControls.gd
@@ -5,10 +5,8 @@ onready var character: Sprite = get_node(characterPath)
 
 func _input(event):
 	if event is InputEventMouseMotion:
-		$HBoxContainer/Value.text = str(event.position)
-		# NOTE: you have to rotate the local position, otherwise you will get strange values that are
-		# relative to the rotate position
-		$HBoxContainer2/Value.text = str(character.get_local_mouse_position().rotated(character.rotation).ceil())
+		$HBoxContainer/Value.text = str(event.position.ceil())  # event.position contains the global mouse position
+		$HBoxContainer2/Value.text = str(character.get_local_mouse_position().ceil())
 
 func _on_Character_angle_changed(new_angle):
 	$HBoxContainer3/Value.text = str(ceil(rad2deg(new_angle))) + "°"
